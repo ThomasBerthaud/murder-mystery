4cefaa0fce76c41c7a2a97513ef9492e7c7bb59a
Thomas Berthaud <thomas.berthaud@parts-advisor.com>
2020-02-11 10:30:32 +0100

    fix badge not properly displayed on small screens

--- src/views/mystery/emails/Emails.vue
+++ src/views/mystery/emails/Emails.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="flex h-full">
     <div class="w-1/5 bg-gray-100 p-2 flex flex-col">
-      <router-link to="/os/emails/received" class="category-title" :class="{ active: isRoute('received') }">
-        <font-awesome-icon :icon="['fas', 'inbox']" fixed-width /> Messages Reçus
+      <router-link to="/os/emails/received" class="category-title with-badge" :class="{ active: isRoute('received') }">
+        <font-awesome-icon :icon="['fas', 'inbox']" fixed-width />
+        <span title="Messages Reçus">Messages Reçus</span>
         <span v-if="nbUnread > 0" class="badge">{{ nbUnread }}</span>
       </router-link>
       <router-link to="/os/emails/sent" class="category-title" :class="{ active: isRoute('sent') }">
-        <font-awesome-icon :icon="['fas', 'paper-plane']" fixed-width /> Messages Envoyés
+        <font-awesome-icon :icon="['fas', 'paper-plane']" fixed-width />
+        <span title="Messages envoyés">Messages Envoyés</span>
       </router-link>
       <router-link to="/os/emails/spam" class="category-title" :class="{ active: isRoute('spam') }">
-        <font-awesome-icon :icon="['fas', 'exclamation-circle']" fixed-width /> Indésirables
+        <font-awesome-icon :icon="['fas', 'exclamation-circle']" fixed-width />
+        <span title="Indésirables">Indésirables</span>
       </router-link>
       <router-link to="/os/emails/trash" class="category-title" :class="{ active: isRoute('trash') }">
-        <font-awesome-icon :icon="['fas', 'trash']" fixed-width /> Corbeille
+        <font-awesome-icon :icon="['fas', 'trash']" fixed-width />
+        <span title="Corbeille">Corbeille</span>
       </router-link>
     </div>
     <div class="w-4/5">
@@ -50,7 +54,11 @@ export default {
   @apply text-white;
   @apply bg-blue-500;
 }
+.with-badge {
+  @apply relative pr-4;
+}
 .badge {
+  @apply absolute top-0 right-0;
   @apply bg-red-500;
   @apply text-white rounded p-1;
 }
